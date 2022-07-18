9d14b7e4f97185bb554c6d37c03efdc356c2c615
Sencer Ilhan <suatsencer12@gmail.com>
2022-07-18 17:48:07 -0400

    updated files

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,19 +1,25 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
 
-export class InfraStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class InfraStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'InfraQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.PYTHON_3_9,    // execution environment
+      code: lambda.Code.fromAsset(path.resolve(__dirname, '/cs-mentor-summer-project-2022/backend')),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
     });
 
-    const topic = new sns.Topic(this, 'InfraTopic');
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
+
