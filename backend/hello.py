e5c0b79130f06ae30419b0e8aa76b2fcf1a4e673
Sencer Ilhan <suatsencer12@gmail.com>
2022-09-01 08:19:02 -0400

    Api Gateway Configuration, and Routing

--- backend/hello.py
+++ backend/hello.py
@@ -1,14 +1,73 @@
+import boto3
+import re
+import logging
 import json
 
 
-def handler(event, context):
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({
-            "Data": "Hello World!"
-        })
+
+def handler(event, context):
+    logger.info(event)
+    httpMethod = event['httpMethod']
+    path = event['path']
+    print(path)
+
+    # GET /users
+    if httpMethod == 'GET' and path == '/users':
+        # responseBody = users.get_all()
+        return buildResponse(200, path)
+
+    # POST /users
+    if httpMethod == 'POST' and path == '/users':
+        # responseBody = users.get_all()
+        return buildResponse(200, [path, json.loads(event['body'])])
+
+    # GET /users/{userId}
+    if httpMethod == 'GET' and re.search(r'/users/(\d+)$', path):
+        user_id = re.search(r'/users/(\d+)', path)[1]
+        return buildResponse(200, [user_id, 'get', event['pathParameters']['userId']])
+
+    # PUT /users/{userId}
+    if httpMethod == 'PUT' and re.search(r'/users/(\d+)$', path):
+        user_id = re.search(r'/users/(\d+)', path)[1]
+        return buildResponse(200, [user_id, 'put', event['pathParameters']['userId'], json.loads(event['body'])])
+
+    # DELETE /users/{userId}
+    if httpMethod == 'DELETE' and re.search(r'/users/(\d+)$', path):
+        user_id = re.search(r'/users/(\d+)', path)[1]
+        return buildResponse(200, [user_id, 'delete', event['pathParameters']['userId']])
+
+    # Get /users/{userId}/sessions
+    if httpMethod == 'GET' and re.search(r'/users/(\d+)/sessions$', path):
+        return buildResponse(200, [path, event['pathParameters']['userId']])
+ # GET /users/{userId}/sessions/{sessionId}
+    if httpMethod == 'GET' and re.search(r'/users/(\d+)/sessions/\d+$', path):
+        return buildResponse(200, [path, event['pathParameters']['userId'], event['pathParameters']['sessionId']])
+
+     # POST /users
+    if httpMethod == 'POST' and re.search(r'/users/(\d+)/sessions$', path):
+        # responseBody = users.get_all()
+        return buildResponse(200, [path, json.loads(event['body'])])
+
+     # PUT /users/{userId}/sessions/{sessionId}
+    if httpMethod == 'PUT' and re.search(r'/users/(\d+)/sessions/\d+$', path):
+        return buildResponse(200, [path, event['pathParameters']['userId'], event['pathParameters']['sessionId'], json.loads(event['body'])])
+
+     # DELETE /users/{userId}/sessions/{sessionId}
+    if httpMethod == 'DELETE' and re.search(r'/users/(\d+)/sessions/\d+$', path):
+        return buildResponse(200, [path, event['pathParameters']['userId'], event['pathParameters']['sessionId']])
+
+
+def buildResponse(statusCode, body=None):
+    response = {
+        'statusCode': statusCode,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
     }
+    if body is not None:
+        response['body'] = json.dumps(body)
+    return response
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,25 +1,86 @@
-import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigw from 'aws-cdk-lib/aws-apigateway';
-import * as path from 'path';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as path from "path";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const usersTable = new dynamodb.Table(this, "Users", {
+      tableName: "FocusTrackerUser",
+      partitionKey: { name: "UserId", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "CreatedAt", type: dynamodb.AttributeType.NUMBER },
+      // UserName
+      // CreatedAt
+      // Email
+    });
+
+    const sessionsTable = new dynamodb.Table(this, "Sessions", {
+      tableName: "FocusTrackerSession",
+      partitionKey: { name: "SessionId", type: dynamodb.AttributeType.STRING },
+      // StartedAt
+      // EndedAt
+      // UserId (global secondary index)
+      // Name
+    });
+
+    // 👇 add global secondary index
+    sessionsTable.addGlobalSecondaryIndex({
+      indexName: 'UserIdIndex',
+      partitionKey: { name: 'UserId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'StartedAt', type: dynamodb.AttributeType.STRING },
+    });
 
     // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.PYTHON_3_9,    // execution environment
-      code: lambda.Code.fromAsset('../backend'),  // code loaded from "lambda" directory
-      handler: 'hello.handler'                // file is "hello", function is "handler"
+    const hello = new lambda.Function(this, "FocusTrackerLambdaFunction", {
+      functionName: "FocusTrackerLambdaFunction",
+      runtime: lambda.Runtime.PYTHON_3_9, // execution environment
+      code: lambda.Code.fromAsset("../backend"), // code loaded from "lambda" directory
+      handler: "hello.handler", // file is "hello", function is "handler"
     });
 
     // defines an API Gateway REST API resource backed by our "hello" function.
-    new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
-    });
+    const api = new apigw.LambdaRestApi(this, "FocusTrackerApiGateway", {
 
+      handler: hello,
+      proxy: false,
+    });
+    // # POST /Users body: {}
+    // # PUT /Users/:id body: {} //Update user info
+    // # DELETE /Users/:id
+    // # GET /Users/:id/Sessions
+
+
+    // # GET /Users/:id/Sessions/:id
+    // # POST /Users/:id/Sessions
+    // # PUT /Users/:id/Sessions/:id //update session info
+    // # DELETE /Users/:id/Sessions/:id
+
+    // /users
+    const users = api.root.addResource("users");
+    users.addMethod("GET");   // Get all users
+    users.addMethod("POST");  // Add a users
+
+    // /users/{userId}
+    const user = users.addResource("{userId}");
+    user.addMethod("GET");    // Get a single user
+    user.addMethod("PUT");    // Update a user
+    user.addMethod("DELETE"); // Delete a user
+
+    // /users/{userId}/sessions
+    const sessions = user.addResource("sessions");
+    sessions.addMethod("GET");  // Get all sessions of a user
+    sessions.addMethod("POST"); // Add a session to a user
+
+
+    // /users/{userId}/sessions
+    const session = sessions.addResource("{sessionId}");
+    session.addMethod("GET");   // Get a single session of a user
+    session.addMethod("PUT");   // Update a session of a user
+    session.addMethod("DELETE");// Delete a session of a user 
+
+    new cdk.CfnOutput(this, "apiUrl", { value: api.url });
   }
 }
-
