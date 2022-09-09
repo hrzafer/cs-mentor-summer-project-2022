a565f163d4264ffa425cb7b30a12137606e2e5d9
hrzafer <harunzafer@gmail.com>
2022-09-09 17:21:25 -0400

    Implement CRUD for users

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -43,9 +43,50 @@ export class InfraStack extends cdk.Stack {
 
     // defines an API Gateway REST API resource backed by our "hello" function.
     const api = new apigw.LambdaRestApi(this, "FocusTrackerApi", {
-
       handler: hello,
+      proxy: false,
     });
+
+     // grant the lambda role read/write permissions to our table
+     usersTable.grantReadWriteData(hello);
+
+     // grant the lambda role read/write permissions to our table
+     sessionsTable.grantReadWriteData(hello);
+
+    // # POST /Users body: {}
+    // # PUT /Users/:id body: {} //Update user info
+    // # DELETE /Users/:id
+    // # GET /Users/:id/Sessions
+
+
+    // # GET /Users/:id/Sessions/:id
+    // # POST /Users/:id/Sessions
+    // # PUT /Users/:id/Sessions/:id //update session info
+    // # DELETE /Users/:id/Sessions/:id
+
+    // /users
+    const users = api.root.addResource("users");
+    users.addMethod("GET");   // Get all users
+    users.addMethod("POST");  // Add a users
+    
+    // /users/{userId}
+    const user = users.addResource("{userId}");
+    user.addMethod("GET");    // Get a single user
+    user.addMethod("PUT");    // Update a user
+    user.addMethod("DELETE"); // Delete a user
+
+    // /users/{userId}/sessions
+    const sessions = user.addResource("sessions");
+    sessions.addMethod("GET");  // Get all sessions of a user
+    sessions.addMethod("POST"); // Add a session to a user
+    
+    
+    // /users/{userId}/sessions
+    const session = sessions.addResource("{sessionId}");
+    session.addMethod("GET");   // Get a single session of a user
+    session.addMethod("PUT");   // Update a session of a user
+    session.addMethod("DELETE");// Delete a session of a user 
+    
 
     new cdk.CfnOutput(this, "apiUrl", { value: api.url });
   }
